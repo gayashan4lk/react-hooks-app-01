94c0bc3c3b3c6de675bb04d392b0d4d8be31be71
Gayashan Wagachchi <gayashan4lk@gmail.com>
2021-06-28 16:11:33 +0530

    added clock component to learn lifecycle

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import './App.css';
 import StateHook from "./Components/StateHook";
 import EffectHook from "./Components/EffectHook";
+import Clock from "./Components/Clock";
 
 const App = () => {
   return (
       <div className="app">
         <StateHook value={10} />
         <EffectHook />
+        <Clock />
       </div>
   );
 }
---
+++ src/Components/Clock.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Clock extends Component {
+    render() {
+        return (
+            <div className="clock-container">
+                <p>sdlse</p>
+            </div>
+        );
+    }
+}
+export default Clock;
